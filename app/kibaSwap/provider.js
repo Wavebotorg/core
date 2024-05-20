ff43f6e81dd8e5daa0381e26e0b758e544b1ab1b
codebyMoh <codebymohit@gmail.com>
2024-05-20 15:10:35 +0530

    2 more network added

--- app/kibaSwap/provider.js
+++ app/kibaSwap/provider.js
@@ -23,6 +23,18 @@ function getProvider(chain, chainId) {
     case 56:
       providerUrl = process.env.INFURA_URL_TESTNET_BSC;
       break;
+    case 59144:
+      providerUrl = process.env.INFURA_URL_TESTNET_LINEA;
+      break;
+    case 324:
+      providerUrl = process.env.INFURA_URL_TESTNET_ZKSYNC;
+      break;
+    case 25:
+      providerUrl = process.env.INFURA_URL_TESTNET_CRONOS;
+      break;
+    case 250:
+      providerUrl = process.env.INFURA_URL_TESTNET_FANTOM;
+      break;
 
     default:
       break;
