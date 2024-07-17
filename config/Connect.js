2f10efd80671ca1cfdc8d8efcacc2496225cfa41
codebyMoh <codebymohit@gmail.com>
2024-07-17 13:27:02 +0530

    :

--- config/Connect.js
+++ config/Connect.js
@@ -1,15 +1,8 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.maindb, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology:true,
-// })
-//     .then(() => { console.log("DB Connect Successfully") })
-//     .catch((err) => { console.log("DB Not Connected",err) })
-
-// module.exports = mongoose
-
 const connectDBs = () => {
+  console.log("🚀 ~ connectDBs ~ process.env.mongoDb:", process.env.mongoDb)
+  console.log("🚀 ~ connectDBs ~ process.env.keypairDb:", process.env.keypairDb)
     try {
         //  * add main db link here
         const maindb = mongoose.createConnection(process.env.mongoDb, {});
@@ -17,14 +10,6 @@ const connectDBs = () => {
 
         console.log("DB Connected");
         return { maindb, keypairdb };
-
-        //  * three diff dbs
-        // const mymongodb = mongoose.createConnection(process.env.maindb, {});
-        // const db1 = mongoose.createConnection(process.env.db1, {});
-        // const db2 = mongoose.createConnection(process.env.db2, {});
-        // const db3 = mongoose.createConnection(process.env.db3, {});
-        console.log("DB Connected");
-        return { mymongodb, db1, db2, db3 };
 
   } catch (error) {
     console.error(`Coudn't connect to db.\n Error:${error.message}`);
